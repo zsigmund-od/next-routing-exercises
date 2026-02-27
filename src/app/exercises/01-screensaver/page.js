6eecd222e66e4fcefc4c5d80515b8da16495947a
zsigmund-od <zsigmund@overdrive.com>
2026-02-27 13:52:12 -0500

    Move screensaver to dynamic segment and make index page for it

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,12 @@
-import React from 'react';
-
-import ScreenSaver from '../../../components/ScreenSaver';
-
-function ScreenSaverExercise() {
-  return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
-    </main>
-  );
+export default function ScreenSaverColorPicker() {
+    return (
+        <main>
+            <h1>Pick a color for the screen saver</h1>
+            <ul>
+                <li><a href="/exercises/01-screensaver/crimson">Crimson</a></li>
+                <li><a href="/exercises/01-screensaver/orchid">Orchid</a></li>
+                <li><a href="/exercises/01-screensaver/teal">Teal</a></li>
+            </ul>
+        </main>
+    )
 }
-
-export default ScreenSaverExercise;
