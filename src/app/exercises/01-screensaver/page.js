eed8e93abaf98366c4c4e7af49325dc31e0addb2
zsigmund-od <zsigmund@overdrive.com>
2026-02-27 13:55:21 -0500

    Dang, I forgot Link already

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,12 +1,20 @@
+import Link from "next/link";
+
 export default function ScreenSaverColorPicker() {
-    return (
-        <main>
-            <h1>Pick a color for the screen saver</h1>
-            <ul>
-                <li><a href="/exercises/01-screensaver/crimson">Crimson</a></li>
-                <li><a href="/exercises/01-screensaver/orchid">Orchid</a></li>
-                <li><a href="/exercises/01-screensaver/teal">Teal</a></li>
-            </ul>
-        </main>
-    )
+  return (
+    <main>
+      <h1>Pick a color for the screen saver</h1>
+      <ul>
+        <li>
+          <Link href="/exercises/01-screensaver/crimson">Crimson</Link>
+        </li>
+        <li>
+          <Link href="/exercises/01-screensaver/orchid">Orchid</Link>
+        </li>
+        <li>
+          <Link href="/exercises/01-screensaver/teal">Teal</Link>
+        </li>
+      </ul>
+    </main>
+  );
 }
